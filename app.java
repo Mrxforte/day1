46b2ed2b695716a2bbd0f859ed6ba26ed6d3e0f4
MrxForte <urazimbetovazamat6@gmail.com>
2026-05-26 13:47:24 -0700

    first commit

---
+++ app.java
@@ -0,0 +1,10 @@
+
+class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        var index = 0;
+        var text = "This is a sample text.";
+        var symbolAt = text.charAt(index);
+        System.out.println("Symbol at index " + index + ": " + symbolAt);
+    }
+}
